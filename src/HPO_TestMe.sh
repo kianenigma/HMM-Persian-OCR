914cb10f3fe061f932a5e432ed672e6d8ed87257
kian.peymani@gmail.com <kian.peymani@gmail.com>
2016-01-12 17:36:24 +0330

    TestMe Script added

--- src/HPO_TestMe.sh
+++ src/HPO_TestMe.sh
@@ -1,8 +1,8 @@
 if [ $# -ne 2 ]; then
-  echo "Usage: testme.sh <text> "
+  echo "Usage: testme.sh <text> <m-state>"
   exit 1
 fi
-pango-view --text "$2" --no-display --font="B Mitra" --dpi=700 --align left --output temp.png
+pango-view --text "$1" --no-display --font="B Mitra" --dpi=700 --align left --output temp.png
 pngtopnm temp.png > temp.pnm
 rm temp.png
 ppmtopgm temp.pnm > temp.pgm 
@@ -21,3 +21,5 @@ pgmtextfea -c 20  > _tmp.fea
 pfl2htk _tmp.fea temp.fea
 
 rm temp.pbm _tmp.fea
+HVite -A -T 1 -o ST -p -17 -s 50 -H hmm/hmm_$2/Macros_hmm -l "*" -i res_single.mlf -w Network Dictionary HMMsList temp.fea
+echo "Result stored in res_single.mlf"
